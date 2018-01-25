3e4fc53e1268e3e5d36403c806e641dbc1f44f62
tim <tim@Neutrino>
2018-01-25 07:31:52 -0700

    fir commit

---
+++ src/bellringerjan25/BellRingerJan25.java
@@ -0,0 +1,23 @@
+
+package bellringerjan25;
+
+/**
+ * Create a program that 
+ * 
+ * 1.  Implements the interface NewString
+ * 2.  Asks the person for a string on the command line
+ * 3.  Executes each one of the methods of your NewString implementation and
+ *     prints the result on the command line
+ * 
+ * @author tim
+ */
+public class BellRingerJan25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/bellringerjan25/NewString.java
@@ -0,0 +1,47 @@
+
+package bellringerjan25;
+
+/**
+ *
+ * @author tim
+ */
+public interface NewString {
+    
+    /**
+     * Reverses the characters in the string
+     * 
+     * e.g. tim would become mit
+     * 
+     * @return 
+     */
+    public String flipped();
+    
+    
+    /**
+     * Randomizes the order of the string
+     * 
+     * e.g. elephant may become hpentaele
+     * 
+     * @return 
+     */
+    public String randomize();
+    
+    
+    /**
+     * Returns the length of the base string
+     * 
+     * @return 
+     */
+    public int length();
+    
+    /**
+     * Returns the string with every other letter a capital.  The first letter
+     * is small
+     * 
+     * e.g. mississippi becomes mIsSiSsIpPi
+     * 
+     * @return 
+     */
+    public String everyOtherCap();
+    
+}
